7fd5d118c19e5767f8f185c60196513f11c0ab54
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-06-19 12:56:15 +0400

    Adding environment file

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyDtwyr0s0tB9Jl1v_Vymn_WKjUe4EijQl0",
+        authDomain: "smarttask-162a0.firebaseapp.com",
+        projectId: "smarttask-162a0",
+        storageBucket: "smarttask-162a0.firebasestorage.app",
+        messagingSenderId: "600412102494",
+        appId: "1:600412102494:web:011c1e097cc61071d579cf"
+    },
+  };
